c12040305b27f386df073891394f52e14e2a6eff
mo0nchild <happytown77831@gmail.com>
2024-03-03 19:15:05 +0300

    Добавьте файлы проекта.

---
+++ IotTelemetry/HostedServices/HiveMQService.cs
@@ -0,0 +1,35 @@
+﻿
+using HiveMQtt.Client.Options;
+using HiveMQtt.Client;
+
+namespace IotTelemetry.HostedServices
+{
+    public class HiveMQService : IHostedService
+    {
+        protected readonly ILogger<HiveMQService> _logger = default!;
+        protected readonly HiveMQClient mqttClient = default!;
+        public HiveMQService(ILogger<HiveMQService> logger) : base() 
+        {
+            this._logger = logger;
+            this.mqttClient = new HiveMQClient(new HiveMQClientOptions()
+            {
+                ClientId = "supertest",
+                Host = "localhost",
+                Port = 5000,
+            });
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            this._logger.LogInformation($"Started");
+            this.mqttClient.OnMessageReceived += (sender, args) =>
+            {
+                this._logger.LogInformation($"{args.PublishMessage.PayloadAsString}");
+            };
+            var response = await this.mqttClient.ConnectAsync();
+            await this.mqttClient.SubscribeAsync("esp8266/data");
+
+            await this.mqttClient.PublishAsync("esp8266/check", "alive");
+        }
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ IotTelemetry/Program.cs
@@ -0,0 +1,36 @@
+
+using HiveMQtt.Client;
+using HiveMQtt.Client.Options;
+using IotTelemetry.HostedServices;
+
+namespace IotTelemetry
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddHostedService<HiveMQService>();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
